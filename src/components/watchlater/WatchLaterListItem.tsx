82aad78e694da85a662880f47dfe32a283290670
fayzanrj <fayzanrj786@gmail.com>
2024-01-01 15:47:27 +0500

    f

--- src/components/watchlater/WatchLaterListItem.tsx
+++ src/components/watchlater/WatchLaterListItem.tsx
@@ -16,10 +16,10 @@ const WatchLaterListItem: React.FC<WatchLaterProps> = ({
 
       {/* Link  and title*/}
       <div className="p-2 select-text overflow-hidden">
-        <p className="font-bold text-[1rem] whitespace-nowrap">
+        <p className="font-bold text-lg sm:text-[1rem] whitespace-nowrap">
           {title.slice(0, 28) + (title.length > 28 ? "...." : "")}
         </p>
-        <p className="text-xs mt-2">
+        <p className="text-sm sm:text-xs mt-2">
           Link :{" "}
           <span>
             <a href={url} className="underline underline-offset-4">
