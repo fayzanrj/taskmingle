f2f9cee01e5801c8dbc56a0b9b1633f25739b9b1
fayzanrj <fayzanrj786@gmail.com>
2024-01-02 19:08:04 +0500

    fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -57,7 +57,7 @@ const Dashboard: NextPage = async () => {
         <DashboardWatchList watchLater={watchLaters} />
 
         {/* Notes */}
-        <iframe style={{borderRadius : "12px"}} src="https://open.spotify.com/embed/album/2r3tG1S4XlpHM3z7xUp2lD?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+       
         {/* <div className="w-80 h-48 bg-[#1f1f1f1]"></div> */}
       </section>
     </div>
