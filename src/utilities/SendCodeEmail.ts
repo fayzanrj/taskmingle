84dcc32ddb3bc9ab1cde832db1784cd7726a3a39
fayzanrj <fayzanrj786@gmail.com>
2024-01-09 10:22:25 +0500

    fix

--- src/utilities/SendCodeEmail.ts
+++ src/utilities/SendCodeEmail.ts
@@ -26,7 +26,32 @@ export const SendCodeEmail = async (
       from: `"Task Notify" <${process.env.EMAIL}>`,
       to: email,
       subject: subject,
-      html: codeTemplateHtml,
+      html: `
+      <div style="text-align: center; font-size: 2xl; color: white; font-weight: bold; letter-spacing: .1rem;">
+        <p>
+          task<span style="color: #19fa9a;">notify</span>
+        </p>
+      </div>
+    
+      <p style="margin-top: 5px; font-weight: bold;">
+        Hi ${name}, hope you are doing great. Here is your verification code.
+      </p>
+    
+      <p style="letter-spacing: .5rem; width: fit-content; margin: 5px auto 5px auto; font-size: 6xl; font-weight: bold;">
+        ${code}
+      </p>
+    
+      <p style="margin-top: 5px; font-weight: bold;">
+        Please keep in mind, without verifying your account you will not be able to get email notifications of your scheduled tasks.
+      </p>
+    
+      <p style="margin-top: 5px; font-weight: bold;">
+        Note:
+        <span style="font-weight: bold;">
+          If you have not requested this code, reply us back on this email ASAP.
+        </span>
+      </p>
+    `,
     });
 
     return emailSent;
