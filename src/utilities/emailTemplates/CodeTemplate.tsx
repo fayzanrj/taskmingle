ebc3dfa933a7fdeb5311d78790a1ab8df504d2b0
fayzanrj <fayzanrj786@gmail.com>
2024-01-09 10:11:05 +0500

    c

--- src/utilities/emailTemplates/CodeTemplate.tsx
+++ src/utilities/emailTemplates/CodeTemplate.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Logo from "../../components/Logo";
 import { Tailwind } from "@react-email/tailwind";
-import { Container, Text, Head } from "@react-email/components";
+import { Container, Text, Head, Body } from "@react-email/components";
 import { Montserrat } from "next/font/google";
+import { Html } from "next/document";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -12,48 +13,45 @@ interface CodeTemplateProps {
 }
 export function CodeTemplate(props: CodeTemplateProps) {
   return (
-    <Tailwind
-      config={{
-        theme: {
-          extend: {},
-        },
-      }}
-    >
-      <Head>
-        <title>Code Verification Email</title>
-      </Head>
-      <Container className="w-96 h-fit mx-auto p-2">
-        {/* LOGO */}
-        <div
-          className={`text-center text-2xl dark:text-white font-extrabold tracking-tighter ${montserrat.className}`}
-        >
-          <p>
-            task<span className="logo text-[#19fa9a]">notify</span>
-          </p>
-        </div>
+    <Tailwind>
+      <Html>
+        <Head />
+        <Body>
+          <Container className="w-96 h-fit mx-auto p-2">
+            {/* LOGO */}
+            <div
+              className={`text-center text-2xl dark:text-white font-extrabold tracking-tighter ${montserrat.className}`}
+            >
+              <p>
+                task<span className="logo text-[#19fa9a]">notify</span>
+              </p>
+            </div>
 
-        <Text className="mt-5 font-semibold">
-          Hi {props.name}&#44; hope you are doing great&#46; Here is your
-          verification code.
-        </Text>
-        <Text
-          style={{ letterSpacing: ".5rem" }}
-          className="w-fit mx-auto my-5 text-6xl font-bold"
-        >
-          {props.code}
-        </Text>
-        <Text className="mt-5 font-semibold">
-          Please keep in mind&#44; without verifying your account you will not
-          be able to get email notifications of your scheduled tasks&#46;
-        </Text>
-        <Text className="mt-5 font-bold">
-          Note :{" "}
-          <Text className="font-semibold">
-            If you have not requested this code&#44; reply us back on this email
-            ASAP
-          </Text>
-        </Text>
-      </Container>
+            <Text className="mt-5 font-semibold">
+              Hi {props.name}&#44; hope you are doing great&#46; Here is your
+              verification code.
+            </Text>
+            <Text
+              style={{ letterSpacing: ".5rem" }}
+              className="w-fit mx-auto my-5 text-6xl font-bold"
+            >
+              {props.code}
+            </Text>
+            <Text className="mt-5 font-semibold">
+              Please keep in mind&#44; without verifying your account you will
+              not be able to get email notifications of your scheduled
+              tasks&#46;
+            </Text>
+            <Text className="mt-5 font-bold">
+              Note :{" "}
+              <Text className="font-semibold">
+                If you have not requested this code&#44; reply us back on this
+                email ASAP
+              </Text>
+            </Text>
+          </Container>
+        </Body>
+      </Html>
     </Tailwind>
   );
 }
